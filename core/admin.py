8522086726edf89b336985719b33ec2542ea7566
Daniel Panangian <D.Panangian@campus.lmu.de>
2021-06-10 19:35:09 +0200

    add custom actions to download csv from admin and change group visibility

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,40 @@
 from django.contrib.admin import ModelAdmin, site
-from core.models import MessageModel
+from core.models import MessageModel, Visibility
+import csv
+from django.http import HttpResponse
 
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
 
-class MessageModelAdmin(ModelAdmin):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export selected messages as CSV"
+
+class MessageModelAdmin(ModelAdmin, ExportCsvMixin):
     readonly_fields = ('timestamp',)
     search_fields = ('id', 'body', 'user__username', 'recipient__username')
     list_display = ('id', 'user', 'recipient', 'timestamp', 'characters')
     list_display_links = ('id',)
     list_filter = ('user', 'recipient')
     date_hierarchy = 'timestamp'
+    actions = ["export_as_csv"]
 
 
 site.register(MessageModel, MessageModelAdmin)
+
+
+class GroupAdmin(ModelAdmin, ExportCsvMixin):
+      list_display    = ['group']
+
+site.register(Visibility, GroupAdmin)
