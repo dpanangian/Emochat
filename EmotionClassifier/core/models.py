9174bd137764fb7ac739cf918b0a7a6d09c580eb
calinostoc <calinostoc@protonmail.com>
2021-07-21 21:54:54 +0200

    update model

--- EmotionClassifier/core/models.py
+++ EmotionClassifier/core/models.py
@@ -1,4 +1,4 @@
-
+from django.db.models import Q, Count, OuterRef, Subquery
 from django.contrib.auth.models import User
 from django.db.models import (Model, TextField, DateTimeField, ForeignKey,
                               CASCADE)
@@ -13,7 +13,9 @@ classifier = EmoClassifier()
 
 class MessageModel(Model):
     """
-    This class represents a chat.
+    This class represents a chatcannot import name 'Classifier' from 'torchmoji.classifier'  message. It has a owner (user), timestamp and
+    the message body.
+
     """
     user = ForeignKey(User, on_delete=CASCADE, verbose_name='user',
                       related_name='from_user', db_index=True)
@@ -22,6 +24,7 @@ class MessageModel(Model):
     timestamp = DateTimeField('timestamp', auto_now_add=True, editable=False,
                               db_index=True)
     body = TextField('body')
+    window_size = 4
 
     def __str__(self):
         return str(self.id)
@@ -56,7 +59,13 @@ class MessageModel(Model):
         """
         new = self.id
         self.body = self.body.strip()   # Trimming whitespaces from the body
-        self.body = classifier.classify_emoji(self.body)
+        queryset = MessageModel.objects.all().filter(
+            Q(recipient=self.user, user__username=self.recipient) |
+            Q(recipient__username=self.recipient, user=self.user))[:self.window_size]
+        context = [element.body.strip() for element in queryset]
+        print("Context", context)
+
+        self.body = classifier.classify_emoji(self.body, context)
         super().save(*args, **kwargs)
         if new is None:
             self.notify_ws_clients()
